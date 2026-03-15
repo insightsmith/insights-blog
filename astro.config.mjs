570d4dba3a9c25c42aea829ff5a36da585509c25
insightsmith <151571685+insightsmith@users.noreply.github.com>
2026-03-15 18:59:05 +1000

    Add custom domain: blog.insightsmith.com.au

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://insightsmith.github.io',
-  base: '/insights-blog',
+  site: 'https://blog.insightsmith.com.au',
+  base: '/',
   integrations: [mdx(), tailwind(), react()],
 });
