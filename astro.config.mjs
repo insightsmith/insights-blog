853d7a09d9cb0e966a2eaf7a5e96e26fcc2469de
insightsmith <151571685+insightsmith@users.noreply.github.com>
2026-03-15 19:30:43 +1000

    Update custom domain to blog.insightsforge.com.au

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://blog.insightsmith.com.au',
+  site: 'https://blog.insightsforge.com.au',
   base: '/',
   integrations: [mdx(), tailwind(), react()],
 });
