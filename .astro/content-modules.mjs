9c29e39d7555f78bb866e9f98974f4fe12a941e5
InsightSmith <insightsforge@proton.me>
2026-03-28 18:09:46 +1000

    Bunch of changes to move to Astro6

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,12 @@
+
+export default new Map([
+["src/content/blog/oil-shock-iran-war-vs-1970s.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Foil-shock-iran-war-vs-1970s.mdx&astroContentModuleFlag=true")],
+["src/content/blog/power-of-now-eckhart-tolle.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fpower-of-now-eckhart-tolle.mdx&astroContentModuleFlag=true")],
+["src/content/blog/react-testing-library.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Freact-testing-library.mdx&astroContentModuleFlag=true")],
+["src/content/blog/stoic-philosophy-modern-life.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fstoic-philosophy-modern-life.mdx&astroContentModuleFlag=true")],
+["src/content/blog/tensegrity-kurilpa-better-movement.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftensegrity-kurilpa-better-movement.mdx&astroContentModuleFlag=true")],
+["src/content/blog/tolle-on-relationships.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftolle-on-relationships.mdx&astroContentModuleFlag=true")],
+["src/content/blog/welcome.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fwelcome.mdx&astroContentModuleFlag=true")],
+["src/content/links/philosophy-books.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flinks%2Fphilosophy-books.mdx&astroContentModuleFlag=true")],
+["src/content/links/tech-tools.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flinks%2Ftech-tools.mdx&astroContentModuleFlag=true")]]);
+		
